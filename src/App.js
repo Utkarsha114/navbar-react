d70e3e7e219682bc595786c137deb3cd0494267e
Utkarsha114 <98257762+Utkarsha114@users.noreply.github.com>
2024-12-30 17:29:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <a href="" className="logo">
+          Logo
+        </a>
+
+        <nav className="navbar">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Portfolio</a>
+          <a href="/">Services</a>
+          <a href="/">Contact</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
